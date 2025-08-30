1958ea2859de590ff1672346c72434813f1037f1
Your Name <your.email@example.com>
2025-08-31 02:30:20 +0800

    Remove modules directory from Git tracking

--- tools/luaModule/modules/测试.lua
+++
@@ -1,7 +0,0 @@
-local items = require('模块:Items/Data2')
-local p={}
--- 通过show获取指定页面下的指定属性值
-function p.test(frame)
-    return "测试本地推送11111"
-end
-return p
--- tools/luaModule/modules/测试2.lua
+++
@@ -1,8 +0,0 @@
-local p={}
-
-function p.test2(frame)
-    local A = mw.getCurrentFrame()
-    return "测试文件222222222"
-end
-
-return p
